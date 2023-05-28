68c32ed07f81cca70a5e8733ebcdd44255d0933e
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-05-28 20:08:11 +0000

    useQuery to show posts & useMutation to add posts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import React, { useState } from 'react';
 import './App.css';
+import { POSTS } from './posts.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [newPostTitle, setNewPostTitle] = useState('');
+  const queryClient = useQueryClient()
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS])
+  })
+
+  const newPostMutation = useMutation(
+    title => {
+      const newPost = {
+        id: crypto.randomUUID(),
+        title,
+      };
+      POSTS.push(newPost);
+      return wait(1000).then(() => newPost);
+    },
+    {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"])
+    }
+  })
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>
+  }
+
+    return (
+      <div className='container'>
+        {postsQuery.data.map(post => (
+          <div className="post-container" key={post.id}>{post.title}</div>
+        ))}
+        <div className="input-container">
+          <input
+            type="text"
+            value={newPostTitle}
+            onChange={e => setNewPostTitle(e.target.value)}
+          />
+        </div>
+        <button
+          disabled={newPostMutation.isLoading}
+          onClick={() => {
+            newPostMutation.mutate(newPostTitle);
+            setNewPostTitle('');
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Add New
+        </button>
+      </div>
+    );
+  }
+
+function wait(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration))
 }
 
 export default App;
