2df68453ab146b5141eda2bb33679572086818af
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-05-31 18:18:02 +0000

    login and sign up pages, basic set up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from 'react-router-dom';
 
 export default function Signup() {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
     return (
-        <main id='signup'>
-            <h1>Sign Up</h1>
+        <main id='login'>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Full name</label>
+                <input value={name} name="name" id="name" placeholder="full name" />
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" 
+                placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" 
+                placeholder="*********" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button>Already have an account? <NavLink to="/login">Login here</NavLink></button>
         </main>
     )
 }
